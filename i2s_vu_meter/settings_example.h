b8e11329fe1789694e8591652beb8e48b7038a9e
Aki Salminen <git@salminen.info>
2018-01-29 14:16:45 +0200

    Fix MQTT_SERVER typo. Add wifi password for esp accesspoint.

---
+++ i2s_vu_meter/settings_example.h
@@ -0,0 +1,10 @@
+// WiFi settings
+#define WIFI_SSID "WIFI ssid"
+#define WIFI_PASSWORD "wifipw"
+
+// MQTT settings
+#define MQTT_TOPIC "mqtttopic"
+#define MQTT_SERVER "mqtt.org"
+#define MQTT_PORT 1883
+#define MQTT_USER "user"
+#define MQTT_PASSWORD "pw"
